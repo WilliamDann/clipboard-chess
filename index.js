62fca988f27b448a84021c625f72fd1b89d1b2a6
WilliamDann <s-william.dann@lwtech.edu>
2021-11-29 12:23:46 -0800

    inital commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const app     = express();
+app.use(express.static('public'));
+
+app.listen(8080, 'localhost', () => console.log("Running on localhost:8080"))
