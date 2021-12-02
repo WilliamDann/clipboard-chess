0490edb7996542fbbda9d4dd852630dc10e6b743
WilliamDann <s-william.dann@lwtech.edu>
2021-12-02 15:52:11 -0800

    game/move route

--- routes/game.js
+++ routes/game.js
@@ -1,10 +1,11 @@
 const {Chessgame, generatorID} = require('../chessgame')
+const {Chess} = require('chess.js');
 
 module.exports = (app, db) => {
     app.post("/game", (req, res) => {
         const id = generatorID()
 
-        db.games[id] = new Chessgame('8/8/8/8/8/8/8/8');
+        db.games[id] = new Chessgame('rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1');
 
         res.status(200);
         res.send({id: id});
@@ -51,11 +52,13 @@ module.exports = (app, db) => {
         if (!req.body.gameID) {
             res.status(400);
             res.send("No gameId specified");
+            return;
         }
 
         if (!db.games[req.body.gameID]) {
             res.status(404);
             res.send("Game not found");
+            return;
         }
 
         for (let key of Object.keys(req.body)) {
@@ -65,5 +68,33 @@ module.exports = (app, db) => {
 
         res.status(200);
         res.send("Item Updated.");
+    });
+
+    app.post('/game/move', (req, res) => {
+        if (!req.body.gameID) {
+            res.status(400);
+            res.send("No gameID specified");
+            return;
+        }
+
+        if (!req.body.move) {
+            res.status(400);
+            res.send("No move specified");
+            return;
+        }
+
+        const chessgame = Chess(db.games[req.body.gameID].fenString);
+        const result    = chessgame.move(req.body.move);
+
+        if (!result) {
+            res.status(400);
+            res.send("Invalid move");
+            return;
+        }
+
+        db.games[req.body.gameID] = chessgame.fen()
+
+        res.status(200);
+        res.send("Move made");
     });
 }
