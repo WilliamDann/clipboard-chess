611d4f40522fd8c5e59fa90c852872bbe1f9af90
SouzaClaudia <sclaudiasa@icloud.com>
2021-12-01 16:03:36 -0800

    added generatorID function

--- chessgame.js
+++ chessgame.js
@@ -4,3 +4,10 @@ class Chessgame {
         this.fenString = fenString;
     }
 }
+
+function generatorID() {
+    return '7yxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+  }
