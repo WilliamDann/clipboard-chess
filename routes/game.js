c2bd2b3f9905274e9e922419c8d9830e604fc042
SouzaClaudia <sclaudiasa@icloud.com>
2021-12-01 16:36:04 -0800

    added app.delete

--- routes/game.js
+++ routes/game.js
@@ -27,4 +27,24 @@ module.exports = (app, db) => {
         res.status(200);
         res.send(db.games[req.body.gameID]);
     });
+
+    app.delete('/game', (req, res) => {
+        if (!req.body.gameID) {
+            res.status(400);
+            res.send('No gameID specified');
+            return;
+        }
+    
+        if (!db.games[req.body.gameID]) {
+            res.status(404);
+            res.send("Game not exists");
+            return;
+        }
+    
+        db.games[req.body.gameID] = undefined;
+    
+        res.status(200);
+        res.send(db.games[req.body.gameID]);
+      });
+    
 }
