73d037cac9dab5b88e049aec48fe1e0af2157f96
WilliamDann <s-william.dann@lwtech.edu>
2021-12-01 16:34:25 -0800

    added put

--- routes/game.js
+++ routes/game.js
@@ -27,4 +27,24 @@ module.exports = (app, db) => {
         res.status(200);
         res.send(db.games[req.body.gameID]);
     });
+
+    app.put('/game', (req, res) => {
+        if (!req.body.gameID) {
+            res.status(400);
+            res.send("No gameId specified");
+        }
+
+        if (!db.games[req.body.gameID]) {
+            res.status(404);
+            res.send("Game not found");
+        }
+
+        for (let key of Object.keys(req.body)) {
+            if (key == 'gameID') continue;
+            db.games[req.body.gameID][key] = req.body[key]
+        }
+
+        res.status(200);
+        res.send("Item Updated.");
+    });
 }
