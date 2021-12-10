60209d52db2518df6984813f0020c024f9080c09
WilliamDann <s-william.dann@lwtech.edu>
2021-12-10 11:14:18 -0800

    added cmd flag parsing

--- index.js
+++ index.js
@@ -5,7 +5,28 @@ const app     = express();
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const parseCmdArgs = () => {
+    const args = {}
+
+    for (let i = 0; i < process.argv.length; i++) {
+        if (process.argv[i].indexOf('-') != -1) {
+            if (i+1 > process.argv.length)
+                throw new Error("Invalid args")
+
+            args[process.argv[i].replaceAll('-', '')] = process.argv[i+1]
+        }
+    }
+
+    return args
+}
+
+let PORT   = process.env.PORT;
+const args = parseCmdArgs()
+if (args['port'])
+    PORT = args['port']
+
+
 // TODO save database object
 require('./routes/all')(app, { games: {} })
 
-app.listen(process.env.PORT, () => "Running on " + (process.env.PORT))
+app.listen(PORT, () => console.log("Running on port" + PORT))
