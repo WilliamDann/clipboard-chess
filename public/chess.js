f65f7550611f0c82cfb1794ff8660f8413605de3
WilliamDann <s-william.dann@lwtech.edu>
2021-12-09 17:35:36 -0800

    update side to move

--- public/chess.js
+++ public/chess.js
@@ -27,6 +27,11 @@ async function fetchPosition() {
   document.querySelector('#whitePlayer').innerHTML = 'White Player: ' + position.whitePlayer;
   document.querySelector('#blackPlayer').innerHTML = 'Black Player: ' + position.blackPlayer;
 
+  $status.html(status)
+  document.querySelector('#fen').innerHTML = game.fen();
+  document.querySelector('#pgn').innerHTML = game.pgn()
+  document.querySelector('#game_id').innerHTML = gameID;
+
   displayChatMessages(position.chat);
 
   return position;
@@ -103,11 +108,6 @@ function updateStatus() {
       status += ', ' + moveColor + ' is in check'
     }
   }
-
-  $status.html(status)
-  $fen.html(game.fen())
-  $pgn.html(game.pgn())
-  document.querySelector('#game_id').innerHTML = gameID;
 }
 
 var config = {
